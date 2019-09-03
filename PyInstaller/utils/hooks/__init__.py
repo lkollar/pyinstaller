cdd4bef3c18d6dfba5e0da3423c1a943f0f98a46
Laszlo Kiss-Kollar <kiss.kollar.laszlo@gmail.com>
2019-09-03 12:51:03 +0100

    Avoid adding package dependencies to hiddenimports
    The module graph already contains the transitive dependencies of all
    packages, so it is not necessary to attempt to look up individual
    package dependencies when collecting hidden imports.

--- PyInstaller/utils/hooks/__init__.py
+++ PyInstaller/utils/hooks/__init__.py
@@ -920,59 +920,6 @@ def get_installer(module):
     return None
 
 
-# Walk through every package, determining which distribution it is in.
-def map_distribution_to_packages():
-    logger.info('Determining a mapping of distributions to packages...')
-    dist_to_packages = {}
-    for p in sys.path:
-        # The path entry ``''`` refers to the current directory.
-        if not p:
-            p = '.'
-        # Ignore any entries in ``sys.path`` that don't exist.
-        try:
-            lds = os.listdir(p)
-        except:
-            pass
-        else:
-            for ld in lds:
-                # Not all packages belong to a distribution. Skip these.
-                try:
-                    dist = pkg_resources.get_distribution(ld)
-                except:
-                    pass
-                else:
-                    # I assume a distribution contains only one package.
-                    if (dist.key in dist_to_packages):
-                        logger.info('X'*100)
-                        logger.info('Multiple packages %s in distribution %s',
-                                    [ld] + dist_to_packages[dist.key], dist.key)
-                    dist_to_packages.setdefault(dist.key, []).append(ld)
-
-    return dist_to_packages
-
-
-# Store this mapping as a global, since it is expensive to compute.
-DISTRIBUTION_TO_PACKAGES = map_distribution_to_packages()
-
-
-# Given a ``package_name`` as a string, this function returns a list of packages
-# needed to satisfy the requirements. This output can be assigned directly to
-# ``hiddenimports``.
-def requirements_for_package(package_name):
-    hiddenimports = []
-
-    for requirement in pkg_resources.get_distribution(package_name).requires():
-        if requirement.key in DISTRIBUTION_TO_PACKAGES:
-            required_packages = DISTRIBUTION_TO_PACKAGES[requirement.key]
-            hiddenimports.extend(required_packages)
-        else:
-            logger.warning('Unable to find package for requirement %s from '
-                           'package %s.',
-                           requirement.project_name, package_name)
-
-    return hiddenimports
-
-
 # Given a package name as a string, return a tuple of ``datas, binaries,
 # hiddenimports`` containing all data files, binaries, and modules in the given
 # package. The value of ``include_py_files`` is passed directly to
@@ -986,11 +933,6 @@ def collect_all(package_name, include_py_files=False):
     datas += collect_data_files(package_name, include_py_files)
     binaries = collect_dynamic_libs(package_name)
     hiddenimports = collect_submodules(package_name)
-    try:
-        hiddenimports += requirements_for_package(package_name)
-    except Exception as e:
-        logger.warning('Unable to determine requirements for %s: %s',
-                       package_name, e)
 
     return datas, binaries, hiddenimports
 
